6bc5925af0b00aa197c4480ee9985b2080c38c1c
mbui2003 <126218902+mbui2003@users.noreply.github.com>
2024-02-12 14:51:03 -0800

    Create Ship subclass, CargoShip

---
+++ Polymorphism/CargoShip.java
@@ -0,0 +1,21 @@
+public class CargoShip extends Ship {
+    private int cargoCapacityInTonnage;
+
+    public CargoShip(String shipName, String yearShipBuilt, int cargoCapacityInTonnage) {
+        super(shipName, yearShipBuilt);
+        this.cargoCapacityInTonnage = cargoCapacityInTonnage;
+    }
+
+    public int getCargoCapacityInTonnage() {
+        return cargoCapacityInTonnage;
+    }
+
+    public void setCargoCapacityInTonnage(int cargoCapacityInTonnage) {
+        this.cargoCapacityInTonnage = cargoCapacityInTonnage;
+    }
+
+    public void print() {
+        System.out.println("Ship Name: " + getShipName());
+        System.out.println("Cargo Capacity in Tonnage: " + getCargoCapacityInTonnage());
+    }
+}
