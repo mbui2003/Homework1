51834dc548f8a3948d11e1860e00c03ee729fbd8
mbui2003 <126218902+mbui2003@users.noreply.github.com>
2024-02-12 14:21:57 -0800

    Create Ship subclass, CruiseShip

---
+++ Polymorphism/CruiseShip.java
@@ -0,0 +1,21 @@
+public class CruiseShip extends Ship {
+    private int maximumNumPassengers;
+
+    public CruiseShip(String shipName, String yearShipBuilt, int maximumNumPassengers) {
+        super(shipName, yearShipBuilt);
+        this.maximumNumPassengers = maximumNumPassengers;
+    }
+
+    public int getMaximumNumPassengers() {
+        return maximumNumPassengers;
+    }
+
+    public void setMaximumNumPassengers(int maximumNumPassengers) {
+        this.maximumNumPassengers = maximumNumPassengers;
+    }
+
+    public void print() {
+        System.out.println("Ship Name: " + getShipName());
+        System.out.println("Maximum Number of Passengers: " + getMaximumNumPassengers());
+    }
+}
