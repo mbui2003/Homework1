ce2c80ce9d9c863ddaedeea753a46837f0b16e93
mbui2003 <126218902+mbui2003@users.noreply.github.com>
2024-02-11 21:05:10 -0800

    Create Employee subclass, SalariedEmployee

---
+++ Inheritance/SalariedEmployee.java
@@ -0,0 +1,15 @@
+public class SalariedEmployee extends Employee {
+    private int weeklySalary;
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, int weeklySalary) {
+        super(firstName, lastName, socialSecurityNumber);
+        this.weeklySalary = weeklySalary;
+    }
+
+    public int getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    public void setWeeklySalary(int weeklySalary) {
+        this.weeklySalary = weeklySalary;
+    }
+}
