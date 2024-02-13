30da576fd36f19c25bb6d55b0caacf4e8462c1c8
mbui2003 <126218902+mbui2003@users.noreply.github.com>
2024-02-13 11:38:45 -0800

    Fix spacing

--- Inheritance/SalariedEmployee.java
+++ Inheritance/SalariedEmployee.java
@@ -1,5 +1,6 @@
 public class SalariedEmployee extends Employee {
     private int weeklySalary;
+    
     public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, int weeklySalary) {
         super(firstName, lastName, socialSecurityNumber);
         this.weeklySalary = weeklySalary;
